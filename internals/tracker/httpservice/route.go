2927d1803513050be5cb7d879a1bc6bcd2430e04
bimaaulia15 <bima.aulia@hypefast.id>
2024-01-03 16:56:04 +0700

    refactor route setup

--- internals/tracker/httpservice/route.go
+++ internals/tracker/httpservice/route.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func (h *Handler) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 
-	api.Get("/transactions", h.CreateTransaction)
+	api.Get("/transactions", h.GetTransactions)
 	api.Get("/transactions/summary", h.GetTransactionsSummary)
 	api.Post("/transaction", h.CreateTransaction)
 	api.Delete("/transaction/:id", h.DeleteTransaction)
